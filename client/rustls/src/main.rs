60500dc1c702263891247d8e17f90c1ce973189e
Alan Chung Ma <alanchunggt@gmail.com>
2023-12-05 15:04:13 -0500

    enable rustls client to chose cipher suite

--- client/rustls/src/main.rs
+++ client/rustls/src/main.rs
@@ -16,27 +16,32 @@ fn main() {
     );
     root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
-    let mut config = rustls::ClientConfig::builder()
-        .with_root_certificates(root_store)
-        .with_no_client_auth();
+    let mut crypto_provider = rustls::crypto::ring::default_provider();
+    crypto_provider.cipher_suites = vec![
+        rustls::crypto::ring::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+    ];
 
-    // Allow using SSLKEYLOGFILE.
-    config.key_log = Arc::new(rustls::KeyLogFile::new());
+    let config =
+        rustls::ClientConfig::builder_with_provider(Arc::new(crypto_provider))
+            .with_protocol_versions(rustls::ALL_VERSIONS)
+            .expect("failed smtg")
+            .with_root_certificates(root_store)
+            .with_no_client_auth();
 
     let server_name = "127.0.0.1".try_into().unwrap();
     let mut conn =
         rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
-    let mut sock = TcpStream::connect("127.0.0.1:443").unwrap();
 
     // init and finish handshake
+    let start = std::time::Instant::now();
+    let mut sock = TcpStream::connect("127.0.0.1:443").unwrap();
     let mut stream = rustls::Stream::new(&mut conn, &mut sock);
     stream.flush().unwrap();
+    let duration = start.elapsed();
 
-    let ciphersuite = stream.conn.negotiated_cipher_suite().unwrap();
-    writeln!(
-        &mut std::io::stderr(),
+    println!("Handshake took {:?}", duration);
+    println!(
         "Current ciphersuite: {:?}",
-        ciphersuite.suite()
-    )
-    .unwrap();
+        stream.conn.negotiated_cipher_suite().unwrap().suite()
+    );
 }
