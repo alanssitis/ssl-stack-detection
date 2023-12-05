a71bd729e14a1b0d52a4a6d41d759252ba9cb438
Alan Chung Ma <alanchunggt@gmail.com>
2023-12-05 14:13:47 -0500

    get gotls client working with timing

--- client/gotls/main.go
+++ client/gotls/main.go
@@ -3,12 +3,35 @@ package main
 import (
 	"crypto/x509"
 	"log"
+	"net"
 	"os"
+    "time"
 
 	tls "github.com/refraction-networking/utls"
 )
 
+
+func prependRecordHeader(hello []byte, minTLSVersion uint16) []byte {
+	l := len(hello)
+	if minTLSVersion == 0 {
+		minTLSVersion = tls.VersionTLS10
+	}
+	header := []byte{
+		uint8(22),
+		uint8(minTLSVersion >> 8 & 0xff), uint8(minTLSVersion & 0xff),
+		uint8(l >> 8 & 0xff), uint8(l & 0xff),
+	}
+	return append(header, hello...)
+}
+
+
 func main() {
+    dialConn, err := net.Dial("tcp", "127.0.0.1:443")
+	if err != nil {
+		log.Fatalf("net.Dial() failed: %+v\n", err)
+	}
+    defer dialConn.Close()
+
     dat, err := os.ReadFile("../../go_server/server.crt")
     if err != nil {
         log.Println(err)
@@ -18,16 +41,28 @@ func main() {
     cert_pool := x509.NewCertPool()
     cert_pool.AppendCertsFromPEM(dat)
 
-    conf := &tls.Config{
-        CipherSuites: []uint16{tls.TLS_AES_128_GCM_SHA256},
+    config := &tls.Config{
+        ServerName: "127.0.0.1",
         RootCAs: cert_pool,
     }
+	tlsConn := tls.UClient(dialConn, config, tls.HelloCustom)
+    defer tlsConn.Close()
 
-    conn, err := tls.Dial("tcp", "127.0.0.1:443", conf)
+    err = tlsConn.ApplyPreset(&tls.ClientHelloSpec{
+        TLSVersMax: tls.VersionTLS13,
+		TLSVersMin: tls.VersionTLS10,
+		CipherSuites: []uint16{
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+    })
+
+	start := time.Now()
+	err = tlsConn.Handshake()
     if err != nil {
-        log.Fatalln(err)
+        log.Fatalf("tlsConn.Handshake() failed: %+v\n", err)
     }
-    defer conn.Close()
+	elapsed := time.Since(start)
+	log.Printf("Handshake took %s\n", elapsed)
 
-    conn.Handshake()
+    log.Printf("Current ciphersuite: %s\n", tls.CipherSuiteName(tlsConn.HandshakeState.ServerHello.CipherSuite))
 }
